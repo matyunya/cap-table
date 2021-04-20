b7ba34982bea3311d7a8b67fc56fae7b75c9864c
Maxim Matyunin <matyunya@gmail.com>
2021-04-20 13:19:35 +0900

    fix wrapper

--- components/signup/Wrapper.svelte
+++ components/signup/Wrapper.svelte
@@ -1,4 +1,4 @@
-<div class="w-full h-full min-h-screen flex items-center justify-center z-20 text-black dark:text-white mt-12">
+<div class="w-full h-full min-h-screen flex items-center justify-center z-20 text-black dark:text-white">
   <div class="transition duration-200 w-128 z-20 p-4 border rounded py-8 break-words px-12 mb-6 antialiased bg-gradient-to-r from-blue-gray-100 via-gray-200 to-warm-gray-200 dark:from-blue-gray-900 dark:via-gray-900 dark:to-warm-gray-800">
     <h1 class="text-center text-3xl font-bold tracking-widest uppercase mb-8">
       Capital Dash
