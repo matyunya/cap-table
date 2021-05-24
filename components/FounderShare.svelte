35cc353d9ac370a22c590e9e7ffe5b5abab4203c
Maxim Matyunin <matyunya@gmail.com>
2021-05-24 11:09:27 +0900

    styles

--- components/FounderShare.svelte
+++ components/FounderShare.svelte
@@ -7,7 +7,7 @@
 
 {#if $founderShare > 0 && $founderShare !== 1}
   <div
-    class="text-xs mr-3 font-mono"
+    class="text-xs mr-3"
     class:text-green-500={$founderShare > 0.5}
     class:text-yellow-500={$founderShare < 0.5 && $founderShare > 0.34}
     class:text-red-300={$founderShare < 0.34}
