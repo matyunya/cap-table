d4aac59451cfa1536f09f4103c70485939135b48
Maxim Matyunin <matyunya@gmail.com>
2021-05-25 18:55:08 +0900

    icons

---
+++ icons/step2.svelte
@@ -0,0 +1,53 @@
+<svg
+  version="1.1"
+  class={$$props.class || ""}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  x="0px"
+  y="0px"
+  viewBox="0 0 100 100"
+  style="enable-background:new 0 0 100 100;"
+  xml:space="preserve"
+>
+  <style type="text/css">
+    .st0 {
+      fill: none;
+      stroke: #4b4b4b;
+      stroke-width: 2.9032;
+      stroke-linecap: round;
+      stroke-linejoin: round;
+      stroke-miterlimit: 10;
+    }
+  </style>
+  <g>
+    <path
+      class="st0"
+      d="M92.1,16.6H7.9c-1.6,0-2.9-1.3-2.9-2.9l0,0c0-1.6,1.3-2.9,2.9-2.9h84.2c1.6,0,2.9,1.3,2.9,2.9l0,0
+		C95,15.3,93.7,16.6,92.1,16.6z"
+    />
+    <path
+      class="st0"
+      d="M92.1,74.7H7.9c-1.6,0-2.9-1.3-2.9-2.9l0,0c0-1.6,1.3-2.9,2.9-2.9h84.2c1.6,0,2.9,1.3,2.9,2.9l0,0
+		C95,73.4,93.7,74.7,92.1,74.7z"
+    />
+    <rect x="10.8" y="16.6" class="st0" width="78.4" height="52.3" />
+    <rect x="45.6" y="5" class="st0" width="8.7" height="5.8" />
+    <rect x="45.6" y="74.7" class="st0" width="8.7" height="20.3" />
+    <polygon class="st0" points="80.5,95 71.8,95 58.7,74.7 67.4,74.7 	" />
+    <polygon class="st0" points="19.5,95 28.2,95 41.3,74.7 32.6,74.7 	" />
+    <path
+      class="st0"
+      d="M34,45.6V25.4c-9.7,0.7-17.4,8.9-17.4,18.8c0,10.4,8.4,18.9,18.9,18.9c9.9,0,18.1-7.7,18.8-17.4L34,45.6
+		L34,45.6z"
+    />
+    <path class="st0" d="M56.8,39.8c-1.7-8.5-8.5-15.3-17-17v17H56.8z" />
+    <circle class="st0" cx="66" cy="25.3" r="2.9" />
+    <circle class="st0" cx="66" cy="48.5" r="2.9" />
+    <line class="st0" x1="74.7" y1="25.3" x2="83.4" y2="25.3" />
+    <line class="st0" x1="74.7" y1="31.1" x2="83.4" y2="31.1" />
+    <line class="st0" x1="74.7" y1="36.9" x2="83.4" y2="36.9" />
+    <line class="st0" x1="74.7" y1="48.5" x2="83.4" y2="48.5" />
+    <line class="st0" x1="74.7" y1="54.4" x2="83.4" y2="54.4" />
+    <line class="st0" x1="74.7" y1="60.2" x2="83.4" y2="60.2" />
+  </g>
+</svg>
---
+++ icons/step3.svelte
@@ -0,0 +1,68 @@
+<svg
+  version="1.1"
+  class={$$props.class || ""}
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  x="0px"
+  y="0px"
+  viewBox="0 0 100 100"
+  style="enable-background:new 0 0 100 100;"
+  xml:space="preserve"
+>
+  <g>
+    <path
+      class="st0"
+      d="M86.6,13.4H50v2.8h30.3c-1.3,1.5-2.2,3.5-2.2,5.6v59.1v2.8v2.8c0,3.1-2.5,5.6-5.6,5.6s-5.6-2.5-5.6-5.6v-2.8
+		v-2.8h-2.8H30.3V64.1h-2.8V81H13.4h-2.8v5.6c0,4.7,3.8,8.4,8.4,8.4h53.4c4.7,0,8.4-3.8,8.4-8.4v-2.8V27.5H92h3v-5.6
+		C95,17.2,91.2,13.4,86.6,13.4z M19.1,92.2c-3.1,0-5.6-2.5-5.6-5.6v-2.8h50.6v2.8c0,2.2,0.8,4.1,2.2,5.6H19.1z M92.2,24.7H80.9v-2.8
+		c0-3.1,2.5-5.6,5.6-5.6s5.6,2.5,5.6,5.6L92.2,24.7L92.2,24.7z"
+    />
+    <path
+      class="st0"
+      d="M47.2,58.4V7.8c0-1.6-1.3-2.8-2.8-2.8H7.8C6.3,5,5,6.3,5,7.8v50.6c0,1.6,1.3,2.8,2.8,2.8h36.6
+		C45.9,61.2,47.2,60,47.2,58.4z M7.8,7.8h36.6v50.6H7.8V7.8z"
+    />
+    <path
+      class="st0"
+      d="M10.6,21.9h30.9V10.6H10.6V21.9z M13.4,13.4h25.3V19H13.4V13.4z"
+    />
+    <rect x="10.6" y="27.5" class="st0" width="5.6" height="2.8" />
+    <rect x="19.1" y="27.5" class="st0" width="5.6" height="2.8" />
+    <rect x="27.5" y="27.5" class="st0" width="5.6" height="2.8" />
+    <rect x="35.9" y="27.5" class="st0" width="5.6" height="2.8" />
+    <rect x="10.6" y="33.1" class="st0" width="5.6" height="2.8" />
+    <rect x="19.1" y="33.1" class="st0" width="5.6" height="2.8" />
+    <rect x="27.5" y="33.1" class="st0" width="5.6" height="2.8" />
+    <rect x="35.9" y="33.1" class="st0" width="5.6" height="2.8" />
+    <rect x="10.6" y="38.8" class="st0" width="5.6" height="2.8" />
+    <rect x="19.1" y="38.8" class="st0" width="5.6" height="2.8" />
+    <rect x="27.5" y="38.8" class="st0" width="5.6" height="2.8" />
+    <rect x="35.9" y="38.8" class="st0" width="5.6" height="2.8" />
+    <rect x="10.6" y="44.4" class="st0" width="5.6" height="2.8" />
+    <rect x="19.1" y="44.4" class="st0" width="5.6" height="2.8" />
+    <rect x="27.5" y="44.4" class="st0" width="5.6" height="2.8" />
+    <rect x="35.9" y="44.4" class="st0" width="5.6" height="2.8" />
+    <rect x="10.6" y="50" class="st0" width="5.6" height="2.8" />
+    <rect x="19.1" y="50" class="st0" width="5.6" height="2.8" />
+    <rect x="27.5" y="50" class="st0" width="5.6" height="2.8" />
+    <rect x="35.9" y="50" class="st0" width="5.6" height="2.8" />
+    <path
+      class="st0"
+      d="M58.4,64.1H35.9v2.8h33.7h2.8V47.2v-2.8v-2.8v-2.8V36v-2.8v-2.8v-2.8H50v2.8h8.4v2.8H50V36h8.4v2.8H50v2.8h8.4
+		v2.8H50v2.8h8.4V64.1z M61.2,30.3h8.4v2.8h-8.4C61.2,33.1,61.2,30.3,61.2,30.3z M61.2,35.9h8.4v2.8h-8.4
+		C61.2,38.7,61.2,35.9,61.2,35.9z M61.2,41.6h8.4v2.8h-8.4C61.2,44.4,61.2,41.6,61.2,41.6z M61.2,47.2h8.4v16.9h-8.4
+		C61.2,64.1,61.2,47.2,61.2,47.2z"
+    />
+  </g>
+</svg>
+
+<style type="text/css">
+  .st0 {
+    fill: none;
+    stroke: #4b4b4b;
+    stroke-width: 1.8;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    stroke-miterlimit: 10;
+  }
+</style>
